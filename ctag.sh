9d25af5587d8d42e1a0ef5f7d363a0a986b1968b
Marioexplo <marioretor64@gmail.com>
2025-07-26 22:54:55 +0200

    Added default excluded and exceptions checking

--- ctag.sh
+++ ctag.sh
@@ -1,21 +1,71 @@
 #!/bin/bash
+command -v eyeD3 >/dev/null 2>&1 || {
+    echo "eyeD3 wasn't found. Please install it to use ctag"
+    exit 1
+}
 all=""
-exclude=""
-while getopts ":he:a" opt; do
+exclude=()
+if [ -f "$HOME/.config/ctag" ]; then
+    IFS=',' read -ra exclude <<< "$(sed "1{p;q}" "$HOME/.config/ctag")"; else
+    echo " " > "$HOME/.config/ctag"
+fi
+while getopts ":he:ad:" opt; do
     case $opt in
         h)
             echo "A small CLI interface for Bash to see the tag of all the files in a directory, in particular the artist tag in mp3 files."
             echo "Usage: ctag [OPTIONS] DIRECTORY"
-            echo "  -h                      Print this"
-            echo "  -e [FORMAT,FORMAT...]   Exclude file formats"
-            echo "  -a                      All: paste untagged files, too"
+            echo "  -h                          Print this"
+            echo "  -e FORMAT,FORMAT...         Exclude file formats"
+            echo "  -a                          All: paste untagged files, too"
+            echo "  -d MODE FORMAT,FORMAT...    Default formats to be always exluded"
+            echo "                              MODE can be SET, ADD or SUB"
+            echo "                              Use without DIRECTORY or other flags"
             exit 0
             ;;
         e)
-            IFS=',' read -ra exclude <<< "$OPTARG"
+            IFS=',' read -ra addExcl <<< "$OPTARG"
+            for excl in "${addExcl[@]}"; do
+                exclude+=("$excl")
+            done
             ;;
         a)
-            all="v"
+            all=v
+            ;;
+        d)
+            forms="${!#}"
+            case $OPTARG in
+                SET)
+                    ;;
+                ADD)
+                    forms="${exclude[*]},$forms"
+                    ;;
+                SUB)
+                    IFS=','
+                    read -ra sub <<< "$forms"
+                    forms=()
+                    for form in "${exclude[@]}"; do
+                        add=""
+                        for su in "${sub[@]}"; do
+                            if [ "$form" = "$su" ]; then
+                                add=v
+                            fi
+                        done
+                        if [ -z "$add" ]; then
+                            forms+=("$form")
+                        fi
+                    done
+                    ;;
+                \?)
+                    echo "Unknown MODE $OPTARG"
+                    exit 1
+                    ;;
+            esac
+            for form in "${forms[@]}"; do
+                returner+=$form,
+            done
+            sed -i "1s/.*/${returner}/" "$HOME/.config/ctag"
+            echo "Default exluded: $returner"
+            exit 0
             ;;
         \?)
             echo 'Option not found'
@@ -23,6 +73,15 @@ while getopts ":he:a" opt; do
             ;;
     esac
 done
+shift $((OPTIND -1))
+if [ $# -ne 1 ]; then
+    echo "Use just one directory"
+    exit 1
+fi
+if [[ ! -d $1 ]]; then
+  echo "$1: invalid directory"
+  exit 1
+fi
 names=(Name)
 artists=(Artist)
 n=0
